2b1e246f22e087ad2cfd6b1812f716321cc820c2
Serra Yilmaz <serrayilmaz@sabanciuniv.edu>
2020-07-02 18:37:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for (var i = 0; i < numofDrums; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keypress", function(event){
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -44,4 +46,12 @@ function makeSound(key){
 
     default:
   }
+}
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
